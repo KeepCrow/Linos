c2786b1817351dbe1d441446fb8c6f3a7f637d43
WorthC <wgyforwork@163.com>
2020-10-01 11:14:19 +0800

    显示白屏

--- source/bootpack.c
+++ source/bootpack.c
@@ -1,6 +1,17 @@
+void io_hlt (void);
+void write_mem8 (int addr, int data);
+
 /* 暂时无法修改为LinMain()，可惜可惜 */
 void HariMain(void)
 {
-fin:
-    goto fin;
+    int i;
+    for (i = 0xa0000; i <= 0xaffff; i++)
+    {
+        write_mem8(i, 15);
+    }
+
+    for (;;)
+    {
+        io_hlt();
+    }
 }
